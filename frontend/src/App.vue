e87bc6e8b1c1fcf72146cb22c2e4b711f6036939
Nathawat <natawatpak@gmail.com>
2022-10-17 10:36:10 +0700

    project template
    go + vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3 py-5">
+        <h1>Generate a thumbnail of a Pokemon</h1>
+
+        <form v-on:submit.prevent="makeWebsiteThumbnail">
+          <div class="form-group">
+            <input
+              v-model="websiteUrl"
+              type="text"
+              id="website-input"
+              placeholder="Enter a Pokemon No."
+              class="form-control"
+            />
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary">Generate!</button>
+          </div>
+        </form>
+        <img :src="thumbnailUrl"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+
+  data() {
+    return {
+      websiteUrl: "",
+      thumbnailUrl: "",
+    };
+  },
+
+  methods: {
+    makeWebsiteThumbnail() {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon/" + this.websiteUrl)
+        .then((response) => {
+          this.thumbnailUrl = response.data.sprites.front_default;
+        })
+        .catch((error) => {
+          window.alert(`The API returned an error: ${error}`);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ main/server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./frontend/dist"))
+	http.Handle("/", fs)
+
+	fmt.Println("Server listening on port 3000")
+	log.Panic(
+		http.ListenAndServe(":3000", nil),
+	)
+}
+
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "hello world")
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
