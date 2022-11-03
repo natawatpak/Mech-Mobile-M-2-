842ed1b88d32a9ea93a9feb87105c756e1e7b34c
natawatpak <72492590+natawatpak@users.noreply.github.com>
2022-11-03 20:09:07 +0700

    update map

--- frontend/src/components/CallMech.vue
+++ frontend/src/components/CallMech.vue
@@ -1,19 +1,25 @@
 
 <template>
-  <div class="text-center ma-4">
-    <v-btn @click="locatorButtonPressed()">Get Current location</v-btn>
-    <GMapMap :center="center" :zoom="7" map-type-id="terrain" style="width: 500px; height: 300px">
-      <GMapCluster>
-        <GMapMarker
-          :key="index"
-          v-for="(m, index) in markers"
-          :position="m.position"
-          :clickable="true"
-          :draggable="true"
-          @click="center=m.position"
-        />
-      </GMapCluster>
-    </GMapMap>
+  <div class="pa-5">
+    <div id="map"></div>
+    <v-card variant="tonal" class="text-left pa-4 d-flex justify-left align-center">
+      <v-icon icon="mdi-pin"></v-icon>
+      <section>
+        <v-card-title>Current Location</v-card-title>
+        <v-card-text>{{currentLocation.lat+', '+currentLocation.lng}}</v-card-text>
+      </section>
+    </v-card>
+    <section class="text-center ma-4">
+      <v-btn @click="locatorButtonPressed()">Use Current location</v-btn>
+    </section>
+
+    <v-card variant="tonal" class="text-left pa-4 ">
+      <v-card-title>Car detail</v-card-title>
+      <section>
+        <v-card-text>Type {{car.type}}, Brand {{car.brand}} <br/> License plate: {{car.plate}}</v-card-text>
+      </section>
+    </v-card>
+
   </div>
 </template>
 
@@ -22,16 +28,15 @@
 export default {
   data() {
     return {
-      center: { lat: 51.093048, lng: 6.84212 },
-      markers: [
-        {
-          position: {
-            lat: 51.093048,
-            lng: 6.84212
-          }
-        } // Along list of clusters
-      ]
+      map: null,
+      currentLocation: { lat: 0, lng: 0 },
+      car: {type:"SUV", brand:"MG", plate:"ก2113"}
     };
+  },
+  mounted() {
+    this.initMap();
+    this.locatorButtonPressed();
+    this.setMarker(this.mapCenter, "A");
   },
   methods: {
     locatorButtonPressed() {
@@ -40,6 +45,9 @@ export default {
         const longitude = position.coords.longitude;
         // Do something with the position
         console.log(latitude + ", " + longitude);
+        this.currentLocation.lat = latitude;
+        this.currentLocation.lng = longitude;
+        this.setMarker(this.currentLocation, "A");
       };
 
       const error = err => {
@@ -48,6 +56,28 @@ export default {
 
       // This will open permission popup
       navigator.geolocation.getCurrentPosition(success, error);
+    },
+    initMap() {
+      this.map = new google.maps.Map(document.getElementById("map"), {
+        center: this.currentLocation,
+        zoom: 5,
+        maxZoom: 20,
+        minZoom: 3,
+        streetViewControl: false,
+        mapTypeControl: false,
+        fullscreenControl: true,
+        zoomControl: true
+      });
+    },
+    setMarker(Points, Label) {
+      const markers = new google.maps.Marker({
+        position: Points,
+        map: this.map,
+        label: {
+          text: Label,
+          color: "#FFF"
+        }
+      });
     }
   }
 };
