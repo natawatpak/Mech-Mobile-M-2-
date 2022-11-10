756a2e38a5e8e15b79a2249971244026a65ed96e
natawatpak <72492590+natawatpak@users.noreply.github.com>
2022-11-10 18:34:01 +0700

    .

---
+++ frontend/src/views/AddCarView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <AddCar/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AddCar from '@/components/AddCar.vue'
+
+export default {
+  components: {
+    AddCar
+  }
+}
+</script>
