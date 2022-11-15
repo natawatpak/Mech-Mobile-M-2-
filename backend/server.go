5b5c7c9c66f02cfc3cc2dbdcd1fc3309a617eb2d
natawatpak <72492590+natawatpak@users.noreply.github.com>
2022-11-15 18:36:38 +0700

    add user controller

--- backend/server.go
+++ backend/server.go
@@ -45,48 +45,48 @@ import (
 // 	}, nil
 // }
 
-func main() {
-	r := mux.NewRouter()
+// func main() {
+// 	r := mux.NewRouter()
 
-	var host string = "localhost"
-	var port string = "5432"
-	var user string = "postgres"
-	var password string = "Eauu0244"
-	var dbname string = "postgres"
-	var goChiPort string = "8081"
+// 	var host string = "localhost"
+// 	var port string = "5432"
+// 	var user string = "postgres"
+// 	var password string = "Eauu0244"
+// 	var dbname string = "postgres"
+// 	var goChiPort string = "8081"
 
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+// 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+// 		"password=%s dbname=%s sslmode=disable",
+// 		host, port, user, password, dbname)
 
-	operator, err := resource.NewDBOperator(psqlInfo)
-	util.CheckErr(err)
-	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Not found", r.RequestURI)
-		http.Error(w, fmt.Sprintf("Not found: %s", r.RequestURI), http.StatusNotFound)
-	})
-	schema := generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: operator}})
-	server := handler.NewDefaultServer(schema)
-	r.Handle("/query", server)
-	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	if runtime_api, _ := os.LookupEnv("AWS_LAMBDA_RUNTIME_API"); runtime_api != "" {
-		adapter := gorillamux.NewV2(r)
-		lambda.Start(adapter.ProxyWithContext)
-	} else {
-		srv := handler.NewDefaultServer(
-			generated.NewExecutableSchema(
-				generated.Config{
-					Resolvers: &graph.Resolver{
-						DB: operator,
-					},
-				},
-			),
-		)
+// 	operator, err := resource.NewDBOperator(psqlInfo)
+// 	util.CheckErr(err)
+// 	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		log.Println("Not found", r.RequestURI)
+// 		http.Error(w, fmt.Sprintf("Not found: %s", r.RequestURI), http.StatusNotFound)
+// 	})
+// 	schema := generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: operator}})
+// 	server := handler.NewDefaultServer(schema)
+// 	r.Handle("/query", server)
+// 	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+// 	if runtime_api, _ := os.LookupEnv("AWS_LAMBDA_RUNTIME_API"); runtime_api != "" {
+// 		adapter := gorillamux.NewV2(r)
+// 		lambda.Start(adapter.ProxyWithContext)
+// 	} else {
+// 		srv := handler.NewDefaultServer(
+// 			generated.NewExecutableSchema(
+// 				generated.Config{
+// 					Resolvers: &graph.Resolver{
+// 						DB: operator,
+// 					},
+// 				},
+// 			),
+// 		)
 
-		r.Handle("/", playground.Handler("GraphQL playground", "/query"))
-		r.Handle("/query", srv)
+// 		r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+// 		r.Handle("/query", srv)
 
-		log.Printf("connect to http://%s:%s/ for GraphQL playground", host, goChiPort)
-		log.Fatal(http.ListenAndServe(":"+goChiPort, r))
-	}
-}
+// 		log.Printf("connect to http://%s:%s/ for GraphQL playground", host, goChiPort)
+// 		log.Fatal(http.ListenAndServe(":"+goChiPort, r))
+// 	}
+// }
---
+++ main/controller/customerController.go
@@ -0,0 +1,56 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Khan/genqlient/graphql"
+	"github.com/natawatpak/Mech-Mobile-M-2-/backend/graph"
+)
+
+func CreateCustomerProfile(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	graphqlClient := graphql.NewClient("http://localhost:8081/query", http.DefaultClient)
+
+	resp, err := graph.CustomerCreate(ctx, graphqlClient, &graph.CustomerCreateInput{
+		FName: r.FormValue("fName"),
+		LName: r.FormValue("lName"),
+		Tel:   r.FormValue("tel"),
+		Email: r.FormValue("email"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp.CustomerCreate.FName)
+
+	// respquery, err := graph.CustomerByID(ctx, graphqlClient, "9fdd0d20-7d45-42b6-afe4-036189f5216b")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(*&respquery.CustomerByID.FName)
+
+}
+
+func UpdateCustomerProfile(w http.ResponseWriter, r *http.Request){
+	r.ParseForm()
+
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	graphqlClient := graphql.NewClient("http://localhost:8081/query", http.DefaultClient)
+
+	resp, err := graph.CustomerUpdateMulti(ctx, graphqlClient, &graph.CustomerUpdateInput{
+		ID: r.FormValue("ID"),
+		FName: r.FormValue("fName"),
+		LName: r.FormValue("lName"),
+		Tel:   r.FormValue("tel"),
+		Email: r.FormValue("email"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp.CustomerUpdateMulti.FName)
+}
