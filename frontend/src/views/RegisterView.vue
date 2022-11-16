0aea99788798274bb17db3075b0d3ceeb6b9691b
natawatpak <72492590+natawatpak@users.noreply.github.com>
2022-11-16 13:45:56 +0700

    add register

---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-form>
+      <InputField
+        value="fName"
+        placeholder="please input your firstname"
+        label=""
+        icon=""
+        hint=""
+        color="#2196F3"
+      />
+      <InputField
+        value="lName"
+        placeholder="please input your firstname"
+        label=""
+        icon=""
+        hint=""
+        color="#2196F3"
+      />
+
+      <v-btn type="submit">submit</v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import InputField from "@/components/InputField.vue";
+
+export default {
+  components: {
+    InputField,
+  },
+};
+</script>
