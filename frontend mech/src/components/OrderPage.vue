7ab44007b807dff7b87bf41d367f73fafe9baeb6
punchnnp <punch.nunnapat@gmail.com>
2022-12-11 14:45:55 +0700

    remove data

--- frontend mech/src/components/OrderPage.vue
+++ frontend mech/src/components/OrderPage.vue
@@ -3,27 +3,21 @@
     <div class="text-left text-h4 my-2">Incoming order</div>
 
     <v-card-text class="pa-0 ma-0">
-      <v-window v-model="tab">
-        <v-window-item value="one">
-          <v-content class="justify-start pa-0">
-            <v-container v-for="item in incoming" :key="item.id" class="pa-0" >
-              <v-card @click="dialog=true" width="100%" variant="tonal" v-if="show" class="text-left pa-0 my-4">
-                <v-card-title class="text-h6">
-                  {{item.cus.fName}} | {{item.car.plate}}
-                  <v-card-subtitle class="pa-0">{{item.date}}</v-card-subtitle>
-                </v-card-title>
-                <v-card-text class="text-h7 pa-0 px-4">
-                  Car {{ item.car.type }} | {{ item.car.brand }} <br />
-                  Problems {{ item.problem }} <br />
-                  {{ item.location.distance }} km
-                </v-card-text>
-                <v-row justify="end" class="pa-6">
-                </v-row>
-              </v-card>
-            </v-container>
-          </v-content>
-        </v-window-item>
-      </v-window>
+      <v-content class="justify-start pa-0">
+        <v-container v-for="item in incoming" :key="item.id" class="pa-0" >
+          <v-card @click="dialog=true" width="100%" variant="tonal" v-if="show" class="text-left pa-0 my-4">
+            <v-card-title class="text-h6">
+              {{item.cus.fName}} | {{item.car.plate}}
+              <v-card-subtitle class="pa-0">{{item.date}}</v-card-subtitle>
+            </v-card-title>
+            <v-card-text class="text-h7 pa-0 px-4">
+              Car {{ item.car.type }} | {{ item.car.brand }} <br />
+              Problems {{ item.problem }} <br />
+              {{ item.location.distance }} km
+            </v-card-text>
+          </v-card>
+        </v-container>
+      </v-content>
     </v-card-text>
 
     <v-dialog v-model="dialog" scrollable class="text-left pa-4">
@@ -86,54 +80,8 @@
 export default {
   data() {
     return {
-      items: [
-        {
-          id: "11111",
-          username: "nunnapat",
-          date: "12 Nov 2022",
-          distance: "2.2",
-          type: "SUV",
-          brand: "MG",
-
-        },
-        {
-          id: "22222",
-          username: "natawat",
-          date: "18 Nov 2022",
-          distance: "0.4",
-          type: "Sedan",
-          brand: "MG",
-          problem: "2",
-        },
-        {
-          id: "33333",
-          username: "kiitiphum",
-          date: "22 Nov 2022",
-          distance: "1.3",
-          type: "Van",
-          brand: "MG",
-        },
-      ],
-      details: {
-        id: "12345",
-        type: "SUV",
-        brand: "MG",
-        plate: "ศง 4727",
-        name: "nunnapat",
-        problem: "2",
-        location: "2.1",
-        status: "Finish",
-      },
-      tab: null,
       dialog: false,
-      dialog2: false,
-      location: {
-        lat: "20",
-        lng: "90"
-      },
-      chips: [ ],
       show: true,
-      problems: ["no battery", "broken motor"],
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rutrum tincidunt arcu sed gravida. Suspendisse mollis ex sed magna viverra, eu tincidunt velit lobortis. Phasellus accumsan mauris est, in pretium orci lacinia in. Nulla in bibendum ex, eu condimentum mauris. Pellentesque quis nisl a justo ultrices molestie ac mattis libero. Aliquam vel sollicitudin quam, vel molestie nunc. Proin dolor dolor, vehicula sed nisl dapibus, semper scelerisque nisl. Sed id neque ac metus efficitur vestibulum. Phasellus quis nibh ac dui molestie fringilla sit amet bibendum sem. Quisque consectetur sit amet nunc ac elementum. Suspendisse vulputate mauris erat, nec tempus dui vulputate.",
       incoming: [ { "car": { "brand": "dgdg", "carID": "fa96015c-8b1c-4f3d-8481-dfc6b54b3476", "plate": "1234", "type": "vfx" }, 
                     "cus": { "cusID": "f67efc77-629d-4672-a753-558b1c0dd250", "fName": "ggg", "lName": "gg" }, 
