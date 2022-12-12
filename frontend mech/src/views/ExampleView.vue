e1a83f458271b8fa41750431bff3cef5cfb26297
punchnnp <punch.nunnapat@gmail.com>
2022-12-12 17:43:56 +0700

    example 1 decoration

--- frontend mech/src/views/ExampleView.vue
+++ frontend mech/src/views/ExampleView.vue
@@ -1,30 +1,78 @@
 <template>
   <div class="pa-4 px-lg-16 px-xl-16 mx-lg-auto mx-xl-auto">
     <div class="text-left text-h4 my-2">Incoming order</div>
-    {{ shopID }}
     <v-card-text class="pa-0 ma-0">
       <v-content class="justify-start pa-0">
         <v-container v-for="t in tickets" :key="t.ticketID" class="pa-0">
-          <v-card width="100%" class="text-left pa-4 rounded-lg elevation-4">
+          <v-card @click="getDetails(t)" width="100%" class="text-left pa-4 rounded-lg elevation-4">
             <v-card-title class="text-h6 pa-0">
               {{t.cus.fName}} | {{t.car.plate}}
-              <v-card-subtitle class="pa-0">{{t.date}}</v-card-subtitle>
             </v-card-title>
+            <v-card-text class="text-subtitle-2 pa-0 py-2">
+              Car type: {{t.car.type}}
+              <br />
+              Car brand: {{t.car.brand}}
+              <br />
+              Total problems: {{splitProblem(t.problem).length}}
+            </v-card-text>
           </v-card>
         </v-container>
       </v-content>
     </v-card-text>
-    <v-card v-for="t in tickets" :key="t.ticketID">
-      <v-card-title>
-        {{ t.ticketID }}
-      </v-card-title>
-      <v-card-text>
-        {{ t }}
-      </v-card-text>
-      <v-card-actions>
-        <v-btn flat @click="acceptTicket(t)">accept</v-btn>
-      </v-card-actions>
-    </v-card>
+
+    <v-dialog v-model="dialog" scrollable class="text-left pa-4">
+      <v-card class="text-left pa-4">
+        <v-card-title class="text-h5 text-center pa-0">Order details</v-card-title>
+        <v-card-text style="height: 100%;" class="ma-0 pa-0">
+          <div class="py-2">
+            <v-text class="text-h6 pa-0">Customer name</v-text>
+            <br>
+            {{(details.cus.fName+' '+details.cus.lName)}}
+          </div>
+          <v-divider></v-divider>
+          <div class="py-2">
+            <v-text class="text-h6 pa-0">Car details</v-text>
+            <br>
+            Type: {{details.car.type}}
+            <br>
+            Brand: {{details.car.brand}}
+            <br>
+            License plate: {{details.car.plate}}
+          </div>
+          <v-divider></v-divider>
+          <div class="py-2">
+            <v-text class="text-h6 pa-0">Location</v-text>
+            <br>
+            Latitude: {{details.location.lat}}
+            <br>
+            Longtitude: {{details.location.lng}}
+          </div>
+          <v-divider></v-divider>
+          <div class="py-2">
+            <v-text class="text-h6 pa-0">Problems</v-text>
+            <br>
+            <li v-for="p in splitProblem(details.problem)" :key="p">{{p}}</li>
+          </div>
+          <v-divider></v-divider>
+          <div class="py-2">
+            <v-text class="text-h6 pa-0">Description</v-text>
+            <br>
+            {{details.description}}
+          </div>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-row class="justify-end pt-2">
+          <v-card-action class="py-4 justify-end">
+            <v-btn @click="dialog=false" class="mx-1" variant="tonal" color="error">
+              Decline
+            </v-btn>
+            <v-btn flat @click="acceptTicket(t)" class="mx-1" color="blue" variant="tonal">
+              Accept
+            </v-btn>
+          </v-card-action>
+        </v-row>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -34,6 +82,9 @@ export default {
     return {
       tickets: [],
       shopID: "",
+      problems: [],
+      dialog: false,
+      details: []
     };
   },
   mounted() {
@@ -63,6 +114,14 @@ export default {
     };
   },
   methods: {
+    splitProblem(p) {
+      return this.problems = p.split(",")
+    },
+    getDetails(t) {
+      this.details = t;
+      console.log(this.details)
+      this.dialog = true;
+    },
     getActiveTicket() {
       this.axios
         .post(this.$backendApi + "shop/get-ticket-list")
