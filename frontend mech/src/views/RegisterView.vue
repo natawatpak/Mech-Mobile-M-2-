d2843f64cfacdb215f2ed58817665bebe8fec74e
punchnnp <punch.nunnapat@gmail.com>
2022-12-12 22:37:37 +0700

    registerView (shop) decoration

--- frontend mech/src/views/RegisterView.vue
+++ frontend mech/src/views/RegisterView.vue
@@ -1,27 +1,67 @@
 <template>
-  <div>
-    Register
-    <v-form>
-      <v-text-field
-        v-model="shopName"
-        placeholder="please input shop name"
-      />
-      <v-text-field
-        v-model="shopTel"
-        placeholder="please input your shop telephone number"
-      />
-      <v-text-field v-model="shopEmail" placeholder="shopEmail" />
-      <v-text-field
-        v-model="lat"
-        placeholder="please input your lat"
-      />
-      <v-text-field
-        v-model="lng"
-        placeholder="please input your lng"
-      />
-
-      <v-btn @click="submitForm()">submit</v-btn>
-    </v-form>
+  <div class="pa-6 px-lg-16 px-xl-16 mx-lg-auto mx-xl-auto justify-center align-center">
+    <v-card class="pa-4 rounded-xl elevation-4">
+      <v-card-title class="text-h5 pa-0">Register</v-card-title>
+      <v-card-subtitle class="text-wrap pa-0 text-subtitle-1">Create shop account - easily find jobs from your customer</v-card-subtitle>
+      <v-form class="py-4">
+        <v-text-field 
+          v-model="shopName" 
+          box 
+          full-width 
+          single-line 
+          variant="outlined"
+          label="Shop name" 
+          color="blue-darken-3" 
+          prepend-inner-icon="mdi-store-outline"
+          placeholder="First name"
+          />
+        <v-text-field 
+          v-model="shopTel" 
+          box 
+          full-width 
+          single-line 
+          variant="outlined"
+          label="Telephone number" 
+          color="blue-darken-3" 
+          prepend-inner-icon="mdi-phone-outline"
+          placeholder="Telephone number"
+        />
+        <v-text-field 
+          v-model="shopEmail" 
+          box 
+          full-width 
+          single-line 
+          variant="outlined"
+          label="Email" 
+          color="blue-darken-3" 
+          prepend-inner-icon="mdi-email-outline"
+          placeholder="Email"
+        />
+        <v-text-field 
+          v-model="lat" 
+          box 
+          full-width 
+          single-line 
+          variant="outlined"
+          label="Shop's latitude" 
+          color="blue-darken-3" 
+          prepend-inner-icon="mdi-map-marker-outline"
+          placeholder="Shop's latitude"
+        />
+        <v-text-field 
+          v-model="lng" 
+          box 
+          full-width 
+          single-line 
+          variant="outlined"
+          label="Shop's longtitude" 
+          color="blue-darken-3" 
+          prepend-inner-icon="mdi-map-marker-outline"
+          placeholder="Shop's longtitude"
+        />
+        <v-btn @click="submitForm()" color="blue-darken-2" block size="large" >Sign up</v-btn>
+      </v-form>
+    </v-card>
   </div>
 </template>
 
